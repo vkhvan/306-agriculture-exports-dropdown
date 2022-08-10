524f15c4ad5c31d52392a3a86f0df35d043723a0
vkhvan <vl.khvan@gmail.com>
2022-08-10 18:49:19 +0000

    replaced data source and updated app.py

--- app.py
+++ app.py
@@ -7,19 +7,17 @@ import pandas as pd
 
 ########### Define your variables ######
 
-tabtitle = 'Old McDonald'
-sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
+tabtitle = 'COVID19 Stats by States'
+sourceurl = 'https://www.kaggle.com/datasets/nightranger77/covid19-state-data'
+githublink = 'https://github.com/vkhvan/306-agriculture-exports-dropdown'
 # here's the list of possible columns to choose from.
-list_of_columns =['total exports', 'beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_columns =['Tested','Infected','Deaths']
 
 
 ########## Set up the chart
 
 import pandas as pd
-df = pd.read_csv('assets/usa-2011-agriculture.csv')
+df = pd.read_csv('../assets/COVID19_state.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -30,14 +28,14 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1('2011 Agricultural Exports, by State'),
+    html.H1('COVID19 Stats, by State'),
     html.Div([
         html.Div([
                 html.H6('Select a variable for analysis:'),
                 dcc.Dropdown(
                     id='options-drop',
                     options=[{'label': i, 'value': i} for i in list_of_columns],
-                    value='corn'
+                    value='Infected'
                 ),
         ], className='two columns'),
         html.Div([dcc.Graph(id='figure-1'),
@@ -54,12 +52,12 @@ app.layout = html.Div(children=[
 @app.callback(Output('figure-1', 'figure'),
              [Input('options-drop', 'value')])
 def make_figure(varname):
-    mygraphtitle = f'Exports of {varname} in 2011'
+    mygraphtitle = f'COVID19 cases of {varname}'
     mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-    mycolorbartitle = "Millions USD"
+    mycolorbartitle = "# of cases"
 
     data=go.Choropleth(
-        locations=df['code'], # Spatial coordinates
+        locations=df['Abbr'], # Spatial coordinates
         locationmode = 'USA-states', # set of locations match entries in `locations`
         z = df[varname].astype(float), # Data to be color-coded
         colorscale = mycolorscale,
